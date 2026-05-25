c2526ddc13d020d7de9352dca269cf92ff75187c
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-05-25 15:32:53 +0530

    feat: add global rust application state

--- desktop-app/src-tauri/src/commands/system.rs
+++ desktop-app/src-tauri/src/commands/system.rs
@@ -1,4 +1,14 @@
+use crate::state::app_state::AppState;
+
 #[tauri::command]
-pub fn greet(name: &str) -> String {
-    format!("OpenBridge backend connected: {}", name)
+pub fn greet(name: &str, state: tauri::State<AppState>) -> String {
+    let mut devices = state.connected_devices.lock().unwrap();
+
+    devices.push(name.to_string());
+
+    format!(
+        "OpenBridge backend connected: {} | Total Devices: {}",
+        name,
+        devices.len()
+    )
 }
--- desktop-app/src-tauri/src/lib.rs
+++ desktop-app/src-tauri/src/lib.rs
@@ -1,10 +1,17 @@
 mod commands;
+mod state;
 
 use commands::system::greet;
+use state::app_state::AppState;
+
+use std::sync::Mutex;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState {
+            connected_devices: Mutex::new(vec![]),
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
